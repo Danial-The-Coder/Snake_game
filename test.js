9dccd8c7c78a568e55f2ebc5702f4606a5be5bac
Danial <danialrezk12@gmail.com>
2026-05-31 18:10:43 +0300

    Making apple edible

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
 console.log("Hello world")
+let car1 = {
+    type : "Tesla",
+    color : "White"
+}
+console.log(car1)
+class Car{
+    constructor(type,color){
+        this.type = type;
+        this.color = color;
+    }
+}
+let car2 = new Car("Tesla","White")
+
+console.log(car2.type)
